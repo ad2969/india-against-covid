dca88ed6ee93477db1da9bc0acfd1d5a44df479d
ad2969 <clarenceadrian@alumni.ubc.ca>
2021-05-13 22:49:09 -0700

    setup firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { db } from './services/firebase';
 
 ReactDOM.render(
   <React.StrictMode>
