a96691c08fd2b825cce265058d254c1e04f72337
ad2969 <clarenceadrian@alumni.ubc.ca>
2021-05-13 22:50:59 -0700

    add firebase database url

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -7,7 +7,8 @@ const CONFIG = {
     storageBucket: "india-against-covid-4cf30.appspot.com",
     messagingSenderId: "914519607471",
     appId: "1:914519607471:web:55075b4635fe20bd95ac5a",
-    measurementId: "G-YS18G3VMKR"
+    measurementId: "G-YS18G3VMKR",
+    databaseURL: "https://india-against-covid-4cf30-default-rtdb.firebaseio.com/"
 };
 
 firebase.initializeApp(CONFIG);
