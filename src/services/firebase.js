65221ba9327e23af9c67fd190082d807fcd192c9
ad2969 <clarenceadrian@alumni.ubc.ca>
2021-05-13 22:49:09 -0700

    setup firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const CONFIG = {
+    apiKey: "AIzaSyCaBY6wZvVQ8C3MxfdyjP67q-cU1yhkI68",
+    authDomain: "india-against-covid-4cf30.firebaseapp.com",
+    projectId: "india-against-covid-4cf30",
+    storageBucket: "india-against-covid-4cf30.appspot.com",
+    messagingSenderId: "914519607471",
+    appId: "1:914519607471:web:55075b4635fe20bd95ac5a",
+    measurementId: "G-YS18G3VMKR"
+};
+
+firebase.initializeApp(CONFIG);
+export const db = firebase.database();
