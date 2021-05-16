935ec34f8cfaaff2650848b8b5223630d883c1ee
ad2969 <clarenceadrian@alumni.ubc.ca>
2021-05-15 19:49:10 -0700

    firebase api cleaning

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -9,25 +9,33 @@ function Home() {
 
     const [regions, setRegions] = useState({})
     const [charities, setCharities] = useState({})
+    const [selectedRegion, setSelectedRegion] = useState(null)
 
-    const [firebaseError, setFirebaseError] = useState(false)
+    const [error, setError] = useState(false)
     const [dataLoaded, setDataLoaded] = useState(false)
 
-    const fetchAllData = async (region = null) => {
+    const fetchAllData = async (regionId = null) => {
         try {
             const regions_response = await fetchRegions()
             setRegions(regions_response)
-            console.log({ regions_response })
 
-            if(region) {
-                console.log('searching for region ', region)
-                const charitiesInRegion = await fetchRegionCharities(region)
-                setCharities(charitiesInRegion)
+            if(regionId) {
+                console.log(`searching for region "${regionId}"`)
+                const regionInfo = regions_response[regionId]
+                setSelectedRegion(regionInfo)
+                console.log(`- "${regionInfo.name}" found!`)
+
+                if(regionInfo) {
+                    const charitiesInRegion = await fetchRegionCharities(regionId)
+                    setCharities(charitiesInRegion)
+                }
+            } else {
+                console.log('no region selected')
             }
 
             setDataLoaded(true)
         } catch(err) {
-            setFirebaseError(true)
+            setError(true)
             console.error(err)
         }
     }
@@ -36,13 +44,26 @@ function Home() {
         // get search query, if any
         const urlParams = new URLSearchParams(location.search)
         const regionQuery = urlParams.get('region')
+
+        // fetch all required data
         fetchAllData(regionQuery)
-    }, [])
+    }, [location.search])
 
     return (
         <div>
-            home!
-            Map will be displayed here
+            <div>home! Map will be displayed here</div>
+            <div>Region Data: {JSON.stringify(regions)}</div>
+
+            <br/>
+            {selectedRegion ? <React.Fragment>
+                    <h3>Searching for region {selectedRegion.name}</h3>
+                    <div>Charity Data for Given Region: {JSON.stringify(charities)}</div>
+                </React.Fragment>
+            : null}
+            <br/>
+
+            <div>Data loaded? {dataLoaded ? 'yes' : 'no'}</div>
+            <div>Error found? {error ? 'yes' : 'no'}</div>
         </div>
     )
 }
