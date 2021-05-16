fe1f044ed8c4808330f50cd22b7d63fdcbd5274f
ad2969 <clarenceadrian@alumni.ubc.ca>
2021-05-16 10:11:17 -0700

    add charities

--- src/routes/Admin/Table/index.js
+++ src/routes/Admin/Table/index.js
@@ -4,13 +4,18 @@ import {
     Checkbox,
     Chip,
     IconButton,
+    InputLabel,
+    MenuItem,
     Paper,
+    Select,
+    TextField,
     Toolbar,
     Tooltip,
     Typography,
 
     Dialog,
     DialogActions,
+    DialogContent,
     DialogTitle,
 
     Table,
@@ -21,12 +26,13 @@ import {
     TableRow,
 } from '@material-ui/core'
 import {
+    Add as AddIcon,
     Delete as DeleteIcon,
     Refresh as RefreshIcon
 } from '@material-ui/icons'
 import "./index.css"
 
-import { deleteCharities, deleteRegionFromCharity } from '../../../services/api'
+import { addCharity, deleteCharities, deleteRegionFromCharity } from '../../../services/api'
 
 // sorting
 // regions
@@ -43,6 +49,14 @@ const CharityTable = (props) => {
     } = props
 
     const charitiesList = Object.values(charities)
+    const regionsList = Object.entries(regions)
+
+    const [addCharityDialog, setAddCharityDialog] = useState(false)
+    const [addCharityName, setAddCharityName] = useState('')
+    const [addCharityDescription, setAddCharityDescription] = useState('')
+    const [addCharityRegions, setAddCharityRegions] = useState([])
+    const [addCharityImageUrl, setAddCharityImageUrl] = useState('')
+    const [addCharityRedirectUrl, setAddCharityRedirectUrl] = useState('')
 
     const [selected, setSelected] = useState([])
     const [confirmDeleteSelectedDialog, setConfirmDeleteSelectedDialog] = useState(false)
@@ -81,6 +95,39 @@ const CharityTable = (props) => {
         console.log({ newSelected })
     }
 
+    // ADD CHARITY
+    const handleAddCharity = () => {
+        setAddCharityDialog(true)
+    }
+    const handleAddCharityConfirm = async () => {
+        await addCharity({
+            name: addCharityName,
+            description: addCharityDescription,
+            regions: addCharityRegions,
+            image_url: addCharityImageUrl,
+            redirect_url: addCharityRedirectUrl
+        })
+        setAddCharityDialog(false)
+        await onRefresh()
+        setAddCharityName('')
+        setAddCharityDescription('')
+        setAddCharityRegions([])
+        setAddCharityImageUrl('')
+        setAddCharityRedirectUrl('')
+    }
+    const handleAddCharityCancel = () => {
+        setAddCharityDialog(false)
+        setAddCharityName('')
+        setAddCharityDescription('')
+        setAddCharityRegions([])
+        setAddCharityImageUrl('')
+        setAddCharityRedirectUrl('')
+    }
+    const handleAddCharityRegions = (e) => {
+        const inputRegions = e.target.value
+        setAddCharityRegions(inputRegions)
+    }
+
     // DELETE CHARITIES 
     const handleDeleteCharities = () => {
         setConfirmDeleteSelectedDialog(true)
@@ -105,6 +152,8 @@ const CharityTable = (props) => {
     const handleDeleteRegionConfirm = async () => {
         await deleteRegionFromCharity(confirmDeleteRegionCharityKey, confirmDeleteRegionRegionKey)
         setConfirmDeleteRegionDialog(false)
+        setConfirmDeleteRegionCharityKey(null)
+        setConfirmDeleteRegionRegionKey(null)
         await onRefresh()
     }
     const handleDeleteRegionCancel = () => {
@@ -120,6 +169,7 @@ const CharityTable = (props) => {
                     <React.Fragment>
                         <Typography variant="h6" id="tableTitle" component="div">List of Charities</Typography>
                         <RefreshIcon className="toolbar-button" onClick={onRefresh} />
+                        <AddIcon className="toolbar-button" onClick={handleAddCharity} />
                     </React.Fragment>
                 )}
 
@@ -142,7 +192,7 @@ const CharityTable = (props) => {
                     <TableCell align="right"><b>Description</b></TableCell>
                     <TableCell align="right"><b>Regions</b></TableCell>
                     <TableCell align="right"><b>Image</b></TableCell>
-                    <TableCell align="right"><b>URL</b></TableCell>
+                    <TableCell align="right"><b>Website</b></TableCell>
                 </TableRow>
                 </TableHead>
                 <TableBody>
@@ -178,6 +228,30 @@ const CharityTable = (props) => {
                 </Table>
             </TableContainer>
 
+            {/* Add Charity */}
+            <Dialog open={addCharityDialog} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Add Charity</DialogTitle>
+                <DialogContent>
+                    <TextField autoFocus margin="dense" id="name" label="Name" fullWidth required
+                        value={addCharityName} onChange={(e) => setAddCharityName(e.target.value)} />
+                    <TextField margin="dense" id="description" label="Description" fullWidth required
+                        value={addCharityDescription} onChange={(e) => setAddCharityDescription(e.target.value)} />
+                    <InputLabel style={{ marginTop: '1em' }}>Regions</InputLabel>
+                    <Select margin="dense" id="regions" label="Regions" multiple fullWidth required
+                        value={addCharityRegions} onChange={handleAddCharityRegions} >
+                        {regionsList.map((region) => <MenuItem value={region[0]}>{region[1].name}</MenuItem>)}
+                    </Select>
+                    <TextField margin="dense" id="image_url" label="Image" fullWidth
+                        value={addCharityImageUrl} onChange={(e) => setAddCharityImageUrl(e.target.value)} />
+                    <TextField margin="dense" id="redirect_url" label="Website" fullWidth required
+                        value={addCharityRedirectUrl} onChange={(e) => setAddCharityRedirectUrl(e.target.value)} />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleAddCharityCancel} color="primary">Cancel</Button>
+                    <Button onClick={handleAddCharityConfirm} color="primary">Add</Button>
+                </DialogActions>
+            </Dialog>
+
             {/* Delete Charities */}
             <Dialog
                 disableBackdropClick
@@ -189,7 +263,7 @@ const CharityTable = (props) => {
                 <DialogTitle id="confirmation-dialog-title">Confirm Deleting the Selected Charities?</DialogTitle>
                 <DialogActions>
                     <Button autoFocus onClick={handleDeleteCharitiesCancel} color="primary">Cancel</Button>
-                    <Button onClick={handleDeleteCharitiesConfirm} color="primary">Ok</Button>
+                    <Button onClick={handleDeleteCharitiesConfirm} variant="contained" color="primary">Ok</Button>
                 </DialogActions>
             </Dialog>
 
@@ -206,7 +280,7 @@ const CharityTable = (props) => {
                 </DialogTitle>
                 <DialogActions>
                     <Button autoFocus onClick={handleDeleteRegionCancel} color="primary">Cancel</Button>
-                    <Button onClick={handleDeleteRegionConfirm} color="primary">Ok</Button>
+                    <Button onClick={handleDeleteRegionConfirm} variant="contained" color="primary">Ok</Button>
                 </DialogActions>
             </Dialog>
         </Paper>
