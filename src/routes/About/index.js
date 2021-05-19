3810b58e6f95040d9016433d54bc43b902cdf26e
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-05-19 02:41:41 -0700

    init other pages

--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Header from "../../components/Header/common";
 
-function about () {
+const About = () => {
 	return (
 		<div>
-            about!
+			<Header SCROLL_THRESHOLD={400} logoRedirect={true} />
+			<div style={{ background: "black", height: "400px", width: "100%" }}></div>
+			<div style={{ background: "white", height: "100vh", width: "100%" }}>ABOUT</div>
+			<div style={{ background: "black", height: "400px", width: "100%" }}></div>
 		</div>
 	);
-}
+};
 
-export default about;
+export default About;
