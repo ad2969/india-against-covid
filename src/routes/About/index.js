f759b150b98a4310ae06d596a11fe3667189ba2a
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-05-19 22:27:11 -0700

    small change

--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -7,7 +7,7 @@ const About = () => {
 		<div className="Page">
 			<Header SCROLL_THRESHOLD={400} logoRedirect={true} />
 			<div style={{ background: "black", height: "400px", width: "100%" }}></div>
-			<div style={{ background: "white", height: "100vh", width: "100%" }}>ABOUT</div>
+			<div style={{ background: "white", height: "100vh", width: "100%" }}>ABOUT-CHANGE</div>
 			<div style={{ background: "lightgrey", height: "400px", width: "100%" }}></div>
 			<Footer />
 		</div>
